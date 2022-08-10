434cebd0a72443589e761002fee8f9fb792013c3
Nineteen991 <Nineteen99@gmail.com>
2022-08-09 22:52:49 -0700

    added some input functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header'
@@ -6,13 +7,16 @@ import Watchlist from './routes/Watchlist'
 import './App.css'
 
 export default function App() {
+  const [renderMovie, setRenderMovie] = useState({})
   
   return (
     <>
       <Header />
       
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={
+          <Home renderMovie={renderMovie} setRenderMovie={setRenderMovie}/>
+        } />
         <Route path='/watchlist' element={<Watchlist />} />
       </Routes>
     </>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 import searchIcon from '../images/Icon.png'
+import filmIcon from '../images/Icon-film.png'
 
 const MovieSearchForm = styled.form`
   grid-column: 2 / 3;
@@ -27,13 +29,57 @@ const MovieSearchBtn = styled.button`
   border-radius: 0 6px 6px 0;
 `
 
-export default function Home() {
+const EmptyMovieDiv = styled.div`
+  grid-column: 2 / 3;
+  background: url("${filmIcon}") no-repeat center;
+  /* height: 73vh; */
+  list-style: none;
+  margin-top: 3%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const MovieCard = styled.div`
+  grid-column: 2 / 3;
+  height: 25vh;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+`
+
+export default function Home({renderMovie, setRenderMovie}) {
+  const [inputValue, setInputValue] = useState('')
+  const [movie, setMovie] = useState('')
+
+  function getMovie(e) {
+    e.preventDefault()
+    setMovie(e.target.children[0].value)
+  }
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=b1eb06c0&t=${movie}`)
+      .then(res => res.json())
+      .then(data => setRenderMovie(data))
+  }, [movie])
+console.log(renderMovie)
   return (
     <>
-      <MovieSearchForm>
-        <MovieSearchInput placeholder='Search for a movie' />
+      <MovieSearchForm onSubmit={(e) => getMovie(e)}>
+        <MovieSearchInput 
+          placeholder='Search for a movie'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
         <MovieSearchBtn>Search</MovieSearchBtn>
       </MovieSearchForm>
+      {
+        movie 
+          ? <MovieCard></MovieCard>
+          : <EmptyMovieDiv><h4>Start exploring</h4></EmptyMovieDiv>
+      }
     </>
   )
 }
